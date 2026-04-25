a03dd136b9a806ade4ff473d89c698ceff16e9dd
PBL075 <pavelgorea75@gmail.com>
2026-04-25 12:15:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace benzinaProject
+{
+    class Program
+    {
+        static void Main()
+        {
+          Console.WriteLine("Cati bani ai pentru benzina ? ");
+          double bani = Convert.ToDouble( Console.ReadLine());
+
+          Console.WriteLine("Cati bani costa 1 litru ? ");
+          double costul = Convert.ToDouble (Console.ReadLine ());
+
+          double totalLitri = bani / costul ;
+
+          //double rotunjire = Math.Round (totalLitri , 2);
+          double rotunjire = Math.Floor (totalLitri );
+          //double rotunjire = Math.Ceiling (totalLitri );
+
+          Console.WriteLine(" Tu vei primi " + rotunjire + " litri pentru " + bani + " lei ");
+        }
+    }
+}
